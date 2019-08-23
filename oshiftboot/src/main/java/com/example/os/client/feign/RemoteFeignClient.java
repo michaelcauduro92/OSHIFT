3601c6c39b7954a6f77fabd26c601f1357249dcf
Michael Cauduro <Michael.Cauduro@contractors.cifgroup.com>
2019-08-23 15:10:00 +0200

    added url

--- oshiftboot/src/main/java/com/example/os/client/feign/RemoteFeignClient.java
+++ oshiftboot/src/main/java/com/example/os/client/feign/RemoteFeignClient.java
@@ -4,10 +4,9 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("osbackend")
+@FeignClient(name="osbackend",url="${microservices.osbackend.url}")
 public interface RemoteFeignClient {
 
 	@RequestMapping("oc-backend/getData")
